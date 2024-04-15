e6e62d93588b9c4e87069495598491f767faf7a5
Alhinai99 <162803909+Alhinai99@users.noreply.github.com>
2024-04-15 20:25:25 +0400

    menu
    menu

---
+++ Assets/C_R_MB.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class C_R_MB : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("surface");
+    }
+}
